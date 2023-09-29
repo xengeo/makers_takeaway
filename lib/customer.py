85a4684d42c998ad7d1c8f38caf424961002f970
xengeo <xen.geo4@gmail.com>
2023-09-29 12:33:08 +0100

    add class files

---
+++ lib/customer.py
@@ -0,0 +1,26 @@
+
+class Customer():
+
+    def __init__(self) -> None:
+        pass
+
+
+    def add_item_to_order():
+        pass
+
+
+    def view_order():
+        pass
+
+
+    def confirm_order():
+        pass
+
+
+    def get_receipt():
+        pass
+
+
+    def get_delivery_text():
+        pass
+
