05069d98b930d06aa6a00c258b78ba63398d0db1
xengeo <xen.geo4@gmail.com>
2023-09-29 15:38:44 +0100

    Add item class and test file

---
+++ lib/item.py
@@ -0,0 +1,10 @@
+# File: lib/items.p
+
+class Item():
+    
+    def __init__(self, item, price):
+        # Parameters: Item (string) and price (float)
+        # Returns: Nothing
+        # Side-effects: Sets attributes self.item and self.price
+        pass
+    
